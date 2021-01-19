ad7941a1827a0eefcb972a3268cb67cbbc8327fe
MichaelEclavea <>
2021-01-19 14:40:23 -0800

    register user setup with bcrypt password in usermodel and register router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
 
 
 //------------- AUTH USER ROUTE ---------------//
+router.route('/').post(registerUser)
 router.post('/login', authUser);
 router.route('/profile').get(getUserProfile);
-
 
 export default router
