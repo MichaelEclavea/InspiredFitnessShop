4c16800dd0b0717f513417beabb2523091e2fa1d
MichaelEclavea <>
2021-01-20 12:17:45 -0800

    completed, update-user on profilePage, register new user

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,29 +1,30 @@
-import { CART_ADD_ITEM, CART_DELETE_ITEM } from '../constants/cartConstants'
+import {CART_ADD_ITEM, CART_DELETE_ITEM} from '../constants/cartConstants'
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
-    switch(action.type) {
-        case CART_ADD_ITEM:
-            const item = action.payload
+export const cartReducer = (state = {cartItems: []}, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload
 
-            const existItem = state.cartItems.find(x => x.product === item.product)
-            if(existItem){
-                return {
-                    ...state, 
-                    cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x)
-                }
-            } else {
-                return {
-                    ...state,
-                    cartItems: [...state.cartItems, item]
-                }
-            }
-            case CART_DELETE_ITEM: 
-                return {
-                    ...state, 
-                    cartItems: state.cartItems.filter((x) => x.product !== action.payload)
-                }
-        default: 
-            return state
-
-    }
+      const existItem = state.cartItems.find((x) => x.product === item.product)
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        }
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        }
+      }
+    case CART_DELETE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      }
+    default:
+      return state
+  }
 }
