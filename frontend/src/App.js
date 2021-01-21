7a67ce6d044b60cd7ed7470a24e56a70b9dbd5ad
MichaelEclavea <>
2021-01-21 11:24:36 -0800

    checkout processes complete with step component and final checkout screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import CartScreen from './screens/CartScreen'
 import HomeScreen from './screens/HomeScreen'
+import PaymentScreen from './screens/PaymentScreen'
 import LoginScreen from './screens/LoginScreen'
 import ProductScreen from './screens/ProductScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import { Container } from 'react-bootstrap'
 
 
@@ -19,12 +21,14 @@ function App() {
     <Header/>
     <main className='py-3'>
     <Container>
-    <Route path="/" component={HomeScreen} exact/>
-    <Route path="/login" component={LoginScreen}/>
-    <Route path="/register" component={RegisterScreen}/>
-    <Route path="/profile" component={ProfileScreen}/>
-    <Route path="/product/:id" component={ProductScreen}/>
-    <Route path="/shipping" component={ShippingScreen}/>
+    <Route path='/' component={HomeScreen} exact/>
+    <Route path='/login' component={LoginScreen}/>
+    <Route path='/payment' component={PaymentScreen}/>
+    <Route path='/register' component={RegisterScreen}/>
+    <Route path='/placeorder' component={PlaceOrderScreen}/>
+    <Route path='/profile' component={ProfileScreen}/>
+    <Route path='/product/:id' component={ProductScreen}/>
+    <Route path='/shipping' component={ShippingScreen}/>
     <Route path='/cart/:id?' component={CartScreen}/>
     </Container>
     </main>
