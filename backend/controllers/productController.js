d5879bff00119431f9a5a213943ade80d5ee099a
MichaelEclavea <>
2021-02-01 12:58:21 -0800

    product search box in header working, used regex in controller and passed component searchBox to header

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,14 @@ import Product from '../models/productModel.js'
 
 //------------- FETCH ALL PRODUCTS ---------------//
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+  const keyword = req.query.keyword ? { 
+    name: { 
+      $regex: req.query.keyword,
+      $options: 'i'
+    }
+  } : {}
+
+  const products = await Product.find({ ...keyword })
   res.json(products)
 })
 //---------------- FETCH SINGLE PRODUCT BY PARAM ID------------ //
